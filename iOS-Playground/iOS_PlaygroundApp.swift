e8e99cd7728fd111ca031e0bcb62d9c4a77261da
Prashanna <prashanna.rajbhandari1@gmail.com>
2024-05-27 11:47:51 +0545

    chore: initial commit

---
+++ iOS-Playground/iOS_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_PlaygroundApp.swift
+//  iOS-Playground
+//
+//  Created by Prashanna Rajbhandari on 27/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
